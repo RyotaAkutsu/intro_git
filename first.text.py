e198dd332d30a55476a60ae2927860f072e70df9
RyotaAkutsu <ryota.akutsu000@gmail.com>
2025-08-11 13:20:07 +0900

    initial commit

---
+++ first.text.py
@@ -0,0 +1,20 @@
+print("Hello World!!")
+x=100
+y=200
+z=300
+a=x+y+z
+print(a)
+text1="こんばんは、"
+text2="いい夜ですね"
+text3="今日の夕食はなんですか?"
+print(text1+text2+text3)
+age=22
+text1="私の年齢は"
+text2="歳です。"
+text3=str(age)+text2
+print(text1+text3)
+
+add-a
+
+
+
